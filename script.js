372397583e0feda04f0edb270f9c04a8bae937ea
bolaji babalola <86409452+bbolaji@users.noreply.github.com>
2024-12-14 00:06:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+
+document.getElementById('guests').addEventListener('change', function () {
+    const guestCount = this.value;
+    if (guestCount < 1) {
+        alert("The number of guests must be at least 1.");
+    }
+});
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Thank you for reaching out! We will get back to you shortly.');
 });
+
